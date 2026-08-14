112c863491e9b2c0c6d5dd3622f0044be9e905e1
Jake <j.mitchell7574@gmail.com>
2026-08-14 23:00:05 +0100

    Controller Icons Drawing

--- ArcademiaEngine.Core/Engine/Input/InputGraphics.cs
+++ ArcademiaEngine.Core/Engine/Input/InputGraphics.cs
@@ -24,6 +24,25 @@ public static class InputGraphics
         public Color KeyColor;
     };
 
+    public struct GamepadFaceOptions
+    {
+        public int Height;
+        public Color OutlineColor;
+        public Color InactiveColor;
+        public Color ActiveColor;
+        public AxisDirections Directions;
+    };
+
+    public struct GamepadStickOptions
+    {
+        public int Height;
+        public Color OutlineColor;
+        public Color FillColor;
+        public Color TextColor;
+        public AxisDirections Directions;
+        public char StickLabel;
+    };
+
     [Flags]
     public enum AxisDirections
     {
@@ -169,4 +188,130 @@ public static class InputGraphics
         // Fallback for A-Z, F1-F12, and Android keys (e.g., KeyboardKey.A -> "A")
         _ => key.ToString()
     };
+
+
+    public static void DrawFaceButtons(Vector2 center, GamepadFaceOptions options)
+    {
+        int radius = (int)(options.Height / 3);
+
+        int cX = (int)center.X;
+        int cY = (int)center.Y;
+
+
+        if (options.Directions.HasFlag(AxisDirections.UP))
+            Raylib.DrawCircle(cX, cY - (2 * radius), radius, options.ActiveColor);
+        else
+            Raylib.DrawCircle(cX, cY - (2 * radius), radius, options.InactiveColor);
+
+        if (options.Directions.HasFlag(AxisDirections.DOWN))
+            Raylib.DrawCircle(cX, cY + (2 * radius), radius, options.ActiveColor);
+        else
+            Raylib.DrawCircle(cX, cY + (2 * radius), radius, options.InactiveColor);
+
+        if (options.Directions.HasFlag(AxisDirections.LEFT))
+            Raylib.DrawCircle(cX - (2 * radius), cY, radius, options.ActiveColor);
+        else
+            Raylib.DrawCircle(cX - (2 * radius), cY, radius, options.InactiveColor);
+
+        if (options.Directions.HasFlag(AxisDirections.RIGHT))
+            Raylib.DrawCircle(cX + (2 * radius), cY, radius, options.ActiveColor);
+        else
+            Raylib.DrawCircle(cX + (2 * radius), cY, radius, options.InactiveColor);
+
+        // Outlines
+        Raylib.DrawCircleLines(cX, cY - (2 * radius), radius, options.OutlineColor); // North
+        Raylib.DrawCircleLines(cX, cY + (2 * radius), radius, options.OutlineColor); // South
+        Raylib.DrawCircleLines(cX - (2 * radius), cY, radius, options.OutlineColor); // West
+        Raylib.DrawCircleLines(cX + (2 * radius), cY, radius, options.OutlineColor); // East
+    }
+
+    public static void DrawDirectionalPad(Vector2 center, GamepadFaceOptions options)
+    {
+        float thickness = 2;
+
+        int cX = (int)center.X;
+        int cY = (int)center.Y;
+
+        int borderDist = options.Height / 3; // Distance from center to inner edge
+        int edgeDist = options.Height;       // Distance from center to outer edge
+        int halfDist = options.Height / 2;       // For triangles
+
+        // Backgrounds
+        Raylib.DrawRectangle(cX - edgeDist, cY - borderDist, edgeDist * 2, borderDist * 2, options.InactiveColor);
+        Raylib.DrawRectangle(cX - borderDist, cY - edgeDist, borderDist * 2, edgeDist * 2, options.InactiveColor);
+
+        if (options.Directions.HasFlag(AxisDirections.UP))
+            Raylib.DrawTriangle(new Vector2(cX - borderDist, cY - halfDist), new Vector2(cX + borderDist, cY - halfDist), new Vector2(cX, cY - edgeDist), options.ActiveColor);
+
+        if (options.Directions.HasFlag(AxisDirections.DOWN))
+            Raylib.DrawTriangle(new Vector2(cX, cY + edgeDist), new Vector2(cX + borderDist, cY + halfDist), new Vector2(cX - borderDist, cY + halfDist), options.ActiveColor);
+
+        if (options.Directions.HasFlag(AxisDirections.LEFT))
+            Raylib.DrawTriangle(new Vector2(cX - edgeDist, cY), new Vector2(cX - halfDist, cY + borderDist), new Vector2(cX - halfDist, cY - borderDist), options.ActiveColor);
+
+        if (options.Directions.HasFlag(AxisDirections.RIGHT))
+            Raylib.DrawTriangle(new Vector2(cX + halfDist, cY - borderDist), new Vector2(cX + halfDist, cY + borderDist), new Vector2(cX + edgeDist, cY), options.ActiveColor);
+
+
+        // Outlines
+
+        // Top Direction
+        Raylib.DrawLineEx(new Vector2(cX - borderDist, cY - borderDist), new Vector2(cX - borderDist, cY - edgeDist), thickness, options.OutlineColor);
+        Raylib.DrawLineEx(new Vector2(cX - borderDist, cY - edgeDist), new Vector2(cX + borderDist, cY - edgeDist), thickness, options.OutlineColor);
+        Raylib.DrawLineEx(new Vector2(cX + borderDist, cY - edgeDist), new Vector2(cX + borderDist, cY - borderDist), thickness, options.OutlineColor);
+
+        // Right Direction
+        Raylib.DrawLineEx(new Vector2(cX + borderDist, cY - borderDist), new Vector2(cX + edgeDist, cY - borderDist), thickness, options.OutlineColor);
+        Raylib.DrawLineEx(new Vector2(cX + edgeDist, cY - borderDist), new Vector2(cX + edgeDist, cY + borderDist), thickness, options.OutlineColor);
+        Raylib.DrawLineEx(new Vector2(cX + edgeDist, cY + borderDist), new Vector2(cX + borderDist, cY + borderDist), thickness, options.OutlineColor);
+
+
+        // Bottom Direction
+        Raylib.DrawLineEx(new Vector2(cX - borderDist, cY + borderDist), new Vector2(cX - borderDist, cY + edgeDist), thickness, options.OutlineColor);
+        Raylib.DrawLineEx(new Vector2(cX - borderDist, cY + edgeDist), new Vector2(cX + borderDist, cY + edgeDist), thickness, options.OutlineColor);
+        Raylib.DrawLineEx(new Vector2(cX + borderDist, cY + edgeDist), new Vector2(cX + borderDist, cY + borderDist), thickness, options.OutlineColor);
+
+
+        // Left Direction
+        Raylib.DrawLineEx(new Vector2(cX - borderDist, cY - borderDist), new Vector2(cX - edgeDist, cY - borderDist), thickness, options.OutlineColor);
+        Raylib.DrawLineEx(new Vector2(cX - edgeDist, cY - borderDist), new Vector2(cX - edgeDist, cY + borderDist), thickness, options.OutlineColor);
+        Raylib.DrawLineEx(new Vector2(cX - edgeDist, cY + borderDist), new Vector2(cX - borderDist, cY + borderDist), thickness, options.OutlineColor);
+    }
+
+    public static void DrawStick(Vector2 center, GamepadStickOptions options)
+    {
+        int cX = (int)center.X;
+        int cY = (int)center.Y;
+
+        int stickOutlineEnd = (int)(options.Height / 1.25f); // Distance from center to inner edge
+        int stickOutlineStart = (int)(options.Height / 1.5f); // Distance from center to inner edge
+
+        int triangleStart = stickOutlineEnd + 2; // Distance from center to inner edge
+        int triangleHalfWidth = (int)(options.Height / 3f);       // Distance from center to outer edge
+        int edgeDist = (int)(options.Height * 1.25f);       // Distance from center to outer edge
+
+        int textHeight = ((int)(options.Height / 10) * 10);
+
+        // Stick BG
+        Raylib.DrawCircle(cX, cY, stickOutlineEnd, options.FillColor);
+
+        // Stick Outline
+        Raylib.DrawRing(new Vector2(cX, cY), stickOutlineStart, stickOutlineEnd, 0, 360, 8, options.OutlineColor);
+
+        // Stick Lbale
+        TextUtils.DrawTextAligned($"{options.StickLabel}", textHeight, new Vector2(cX, cY), TextUtils.GuiTextAlignment.Center, TextUtils.GuiTextAlignmentVertical.Middle, options.TextColor);
+
+        if (options.Directions.HasFlag(AxisDirections.UP))
+            Raylib.DrawTriangle(new Vector2(cX - triangleHalfWidth, cY - triangleStart), new Vector2(cX + triangleHalfWidth, cY - triangleStart), new Vector2(cX, cY - edgeDist), options.TextColor);
+
+        if (options.Directions.HasFlag(AxisDirections.DOWN))
+            Raylib.DrawTriangle(new Vector2(cX, cY + edgeDist), new Vector2(cX + triangleHalfWidth, cY + triangleStart), new Vector2(cX - triangleHalfWidth, cY + triangleStart), options.TextColor);
+
+        if (options.Directions.HasFlag(AxisDirections.LEFT))
+            Raylib.DrawTriangle(new Vector2(cX - edgeDist, cY), new Vector2(cX - triangleStart, cY + triangleHalfWidth), new Vector2(cX - triangleStart, cY - triangleHalfWidth), options.TextColor);
+
+        if (options.Directions.HasFlag(AxisDirections.RIGHT))
+            Raylib.DrawTriangle(new Vector2(cX + triangleStart, cY - triangleHalfWidth), new Vector2(cX + triangleStart, cY + triangleHalfWidth), new Vector2(cX + edgeDist, cY), options.TextColor);
+    }
+
 }
--- ArcademiaEngine.Core/Engine/Scenes/ExampleScenes/InputGraphicsPlayground.cs
+++ ArcademiaEngine.Core/Engine/Scenes/ExampleScenes/InputGraphicsPlayground.cs
@@ -10,6 +10,8 @@ public class InputGraphicsPlayground : Scene, ISceneInspector
     Viewport vp;
 
     InputGraphics.KeyboardKeyOptions keyboardOptions;
+    InputGraphics.GamepadFaceOptions gamepadFaceOptions;
+    InputGraphics.GamepadStickOptions gamepadStickOptions;
 
     int pageOption = 0;
 
@@ -24,6 +26,23 @@ public class InputGraphicsPlayground : Scene, ISceneInspector
             LightUp = true,
             TextColor = Color.White,
             KeyColor = Color.Blue
+        };
+
+        gamepadFaceOptions = new()
+        {
+            Height = 15,
+            OutlineColor = Color.Blue,
+            InactiveColor = Color.DarkBlue,
+            ActiveColor = Color.White,
+        };
+
+        gamepadStickOptions = new()
+        {
+            Height = 15,
+            OutlineColor = Color.Blue,
+            FillColor = Color.DarkBlue,
+            TextColor = Color.White,
+            StickLabel = 'R'
         };
     }
 
@@ -70,6 +89,65 @@ public class InputGraphicsPlayground : Scene, ISceneInspector
         }
     }
 
+    public void DrawControllerButtons()
+    {
+        Raylib.DrawText("Controller Graphics", 5, 5, 10, Color.White);
+
+        int maxBitmask = Enum.GetValues(typeof(InputGraphics.AxisDirections))
+                             .Cast<int>()
+                             .Aggregate(0, (acc, val) => acc | val);
+
+        InputGraphics.AxisDirections[] allCombinations = Enumerable.Range(0, maxBitmask + 1)
+                                                     .Select(i => (InputGraphics.AxisDirections)i)
+                                                     .ToArray();
+
+        int currentX = 25;
+        int currentY = 40;
+
+        foreach (var flag in allCombinations)
+        {
+            gamepadFaceOptions.Directions = flag;
+            InputGraphics.DrawFaceButtons(new Vector2(currentX, currentY), gamepadFaceOptions);
+
+            currentX += 45;
+            if (currentX > 550)
+            {
+                currentX = 25; currentY += 45;
+            }
+        }
+
+
+        currentX = 25;
+        currentY += 45;
+
+        foreach (var flag in allCombinations)
+        {
+            gamepadFaceOptions.Directions = flag;
+            InputGraphics.DrawDirectionalPad(new Vector2(currentX, currentY), gamepadFaceOptions);
+
+            currentX += 45;
+            if (currentX > 550)
+            {
+                currentX = 25; currentY += 45;
+            }
+        }
+
+
+        currentX = 25;
+        currentY += 45;
+
+        foreach (var flag in allCombinations)
+        {
+            gamepadStickOptions.Directions = flag;
+            InputGraphics.DrawStick(new Vector2(currentX, currentY), gamepadStickOptions);
+
+            currentX += 45;
+            if (currentX > 550)
+            {
+                currentX = 25; currentY += 45;
+            }
+        }
+    }
 
     public void DrawArcademiaButtons()
     {
@@ -82,5 +160,22 @@ public class InputGraphicsPlayground : Scene, ISceneInspector
         ImGuiEx.RaylibColorEdit("Key Colour", ref keyboardOptions.KeyColor);
         ImGuiEx.RaylibColorEdit("Text Colour", ref keyboardOptions.TextColor);
 
+        ImGui.SeparatorText("Gamepad Face Buttons");
+        ImGuiEx.RaylibColorEdit("Active Button Color", ref gamepadFaceOptions.ActiveColor);
+        ImGuiEx.RaylibColorEdit("Inactive Button Color", ref gamepadFaceOptions.InactiveColor);
+        ImGuiEx.RaylibColorEdit("Outline Color", ref gamepadFaceOptions.OutlineColor);
+
+        ImGui.SeparatorText("Gamepad Stick Options");
+        ImGuiEx.RaylibColorEdit("Stick Fill Color", ref gamepadStickOptions.FillColor);
+        ImGuiEx.RaylibColorEdit("Stick Outline Color", ref gamepadStickOptions.OutlineColor);
+        ImGuiEx.RaylibColorEdit("Stick Text Color", ref gamepadStickOptions.TextColor);
+
+        string a = gamepadStickOptions.StickLabel.ToString();
+        ImGui.InputText("Stick Text Color", ref a, 1);
+
+        if (a.Length > 0)
+            gamepadStickOptions.StickLabel = a.ToCharArray()[0];
+        else
+            gamepadStickOptions.StickLabel = ' ';
     }
 }
